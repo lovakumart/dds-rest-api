a9d4a457f4eeec5bc5101e411162e88400375a75
lovakumart <48078694+lovakumart@users.noreply.github.com>
2019-10-16 01:42:45 -0400

    Email Utility

--- src/main/java/com/org/walmart/dds/config/EmailUtilConfig.java
+++ src/main/java/com/org/walmart/dds/config/EmailUtilConfig.java
@@ -5,8 +5,8 @@ public class EmailUtilConfig {
 	public static final String GMAIL_HOST = "smtp.gmail.com";
 	public static final int SMTP_PORT = 465;
 	public static final boolean SSL_FLAG = true;
-	public static final String WM_CUST_SUPP_EMAIL_ID="kumar.lova@gmail.com";
-	public static final String CUST_EMAIL_ID="kumar.lova@gmail.com";
-	public static final String DEFAULT_USERNAME = "kumar.lova";
-	public static final String DEFAULT_PASSWORD = "Tango1981";
+	public static final String WM_CUST_SUPP_EMAIL_ID="test@gmail.com";
+	public static final String CUST_EMAIL_ID="test@gmail.com";
+	public static final String DEFAULT_USERNAME = "testuser";
+	public static final String DEFAULT_PASSWORD = "password";
 }
